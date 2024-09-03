a470343f1ca04f8e4623076cfcf614fa3262672f
hitesh-mindfire <mfsi.hiteshs@gmail.com>
2024-09-03 16:54:41 +0530

    loading updated and one box added

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,29 +1,9 @@
-// import React from "react";
-// import { Html, useProgress } from "@react-three/drei";
-
-// const LoadingScreen = () => {
-//   const { progress } = useProgress();
-//   console.log(progress, "loading"); // Logs the progress percentage
-
-//   return (
-//     <Html center>
-//       <div className="progress-bar-container" style={{ textAlign: "center" }}>
-//         <label htmlFor="progress-bar">Loading... {progress.toFixed(2)}%</label>
-//         <progress id="progress-bar" value={progress} max="100"></progress>
-//       </div>
-//     </Html>
-//   );
-// };
-
-// export default LoadingScreen;
-
 import React from "react";
 import { Html, useProgress } from "@react-three/drei";
 
 const LoadingScreen = () => {
-  // Use useProgress hook to get loading progress percentage
   const { progress } = useProgress();
-  console.log(progress, "loading"); // Logs the progress percentage to the console
+  console.log(progress, "loading");
 
   return (
     <Html center>
