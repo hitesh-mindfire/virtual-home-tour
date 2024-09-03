82497c83838875e4ae28db3531cba0fccdc23ac9
hitesh-mindfire <mfsi.hiteshs@gmail.com>
2024-09-03 12:18:08 +0530

    loading screen updated

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,16 +1,35 @@
+// import React from "react";
+// import { Html, useProgress } from "@react-three/drei";
+
+// const LoadingScreen = () => {
+//   const { progress } = useProgress();
+//   console.log(progress, "loading"); // Logs the progress percentage
+
+//   return (
+//     <Html center>
+//       <div className="progress-bar-container" style={{ textAlign: "center" }}>
+//         <label htmlFor="progress-bar">Loading... {progress.toFixed(2)}%</label>
+//         <progress id="progress-bar" value={progress} max="100"></progress>
+//       </div>
+//     </Html>
+//   );
+// };
+
+// export default LoadingScreen;
+
 import React from "react";
 import { Html, useProgress } from "@react-three/drei";
 
 const LoadingScreen = () => {
+  // Use useProgress hook to get loading progress percentage
   const { progress } = useProgress();
-  console.log(progress, "loading");
+  console.log(progress, "loading"); // Logs the progress percentage to the console
+
   return (
     <Html center>
-      <div className="loading-container">
-        <div className="spinner"></div>
-        <p style={{ color: "white", fontSize: "24px" }}>
-          Loading... {progress.toFixed(2)}%
-        </p>
+      <div className="progress-bar-container" style={{ textAlign: "center" }}>
+        <label htmlFor="progress-bar">Loading... {progress.toFixed(2)}%</label>
+        <progress id="progress-bar" value={progress} max="100" />
       </div>
     </Html>
   );
